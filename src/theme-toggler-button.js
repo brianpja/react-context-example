589f7216dfc4a5a459ae1eb61854262fd4caa7d3
Brian James <brian.james@slalom.com>
2018-09-14 14:01:22 -0700

    simple context modifier

---
+++ src/theme-toggler-button.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {ThemeContext} from './theme-context';
+
+function ThemeTogglerButton() {
+  // receives theme and toggleTheme function
+  return (
+    <ThemeContext.Consumer>
+      {({theme, toggleTheme}) => (
+        <button
+          onClick={toggleTheme}
+          style={ {backgroundColor: theme.background} }>
+          Toggle Theme
+        </button>
+      )}
+    </ThemeContext.Consumer>
+  );
+}
+
+export default ThemeTogglerButton;
